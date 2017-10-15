b404af80b6f122f147bb092b40601f2e02f0b1d3
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-10-15 16:53:24 +0300

    Математика

---
+++ 05. switches/Математика/Математика/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("В математике функция sign(x) (знак числа) определена так:");
+            Console.WriteLine("Введите знак числа.");
+            int x = int.Parse(Console.ReadLine());
+            if ( x > 0 )
+            {
+                Console.WriteLine("sign(x) = 1");
+            }
+            else if ( x < 0 )
+            {
+                Console.WriteLine("sign(x) = -1");
+            }
+            else if ( x == 0 )
+            {
+                Console.WriteLine("sign(x) = 0");
+            }
+            Console.ReadLine();
+        }
+    }
+}
