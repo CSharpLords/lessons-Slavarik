c0b3f1945ebef40a3bcae50f440781289bf9c0b0
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-10-15 15:49:44 +0300

    Наибольшее число

---
+++ 05. switches/Наибольшое число/Наибольшое число/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите третье число");
+            int c = int.Parse(Console.ReadLine());
+            if (a >= b && a >= c)
+            {
+                Console.WriteLine("Самое большое число:" + a);
+            }
+            else
+            {
+                if (b >= c)
+                {
+                    Console.WriteLine("Самое большое число:" + b);
+                }
+                if (c >= b)
+                {
+                    Console.WriteLine("Самое большое число:" + c);
+                }
+                    
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
