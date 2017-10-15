4d8175e2b028dea73011fd6fe6780d59aafd7694
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-10-15 15:19:00 +0300

    Подросток или нет(упр версия)

---
+++ 05. switches/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваш возраст");
+            int age = int.Parse(Console.ReadLine());
+            if (age >= 10 && age < 20)
+            {
+                Console.WriteLine("Вы подросток");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Вы не подросток");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Скидки/Скидки/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input cash");
+            int cash = int.Parse(Console.ReadLine());
+            int price = 50;
+            int age = 90;
+            int discount = 10;
+            if (age < 9)
+            {
+                price = price - discount;
+            }
+
+            Console.WriteLine("Your cash: " + (cash - price));
+            Console.ReadLine();
+        }
+    }
+}
