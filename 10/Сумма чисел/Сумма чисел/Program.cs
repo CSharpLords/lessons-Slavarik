1a5aa1274516749902e8e284c04aca8d89ac1378
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-11-26 16:24:57 +0300

    Сумма чисел

---
+++ 10/Сумма чисел/Сумма чисел/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите первое число");
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите первое число");
+                int number2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите первое число");
+                int number3 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите первое число");
+                int number4 = int.Parse(Console.ReadLine());
+                int sum = number + number2 + number3 + number4;
+                Console.WriteLine("Сумма чисел ровна:" + sum);
+                Console.WriteLine();
+            }
+        }
+    }
+}
