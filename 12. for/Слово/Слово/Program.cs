6d281844641c6d5f1b93bd71f85c6ae7fdad3ab4
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-12-03 16:12:22 +0300

    Слово

---
+++ 12. for/Слово/Слово/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше,твою мамашу,слово");
+            string word = Console.ReadLine();
+            word = word.ToLower();
+            int a = word.Length - 1;
+            if (word[0] == word[a])
+            {
+                Console.WriteLine("Совпадает.Ты просто Молодец!!!");
+            }
+            else
+            {
+                Console.WriteLine("Несовпадает.Просто кусок тебя.Вот...");
+            }
+            Console.ReadLine();
+        }
+    }
+}
