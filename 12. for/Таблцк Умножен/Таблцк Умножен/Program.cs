16d0c1ad09cb6d50d584bc04c2afb89b71de1dd4
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-12-03 15:25:00 +0300

    Таблцк Умножен

---
+++ 12. for/Таблцк Умножен/Таблцк Умножен/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int account = 1;
+            int b = 7;
+            for (int a = 1; a < 11; a = a + 1)
+            {
+                
+                int c = a * b;
+                Console.WriteLine(account + ".Умножение:" + c);
+                account = account + 1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
