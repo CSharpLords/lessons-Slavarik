3c91d8d37130f527efc3dd18914eec2d339b7018
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-12-03 16:40:52 +0300

    Доллары в рублики

---
+++ 12. for/Доллары в Рублики/Доллары в Рублики/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int dollar = 1; dollar < 21; dollar = dollar + 1)
+            {
+                int Perevod = 60;
+                int Rub = dollar * Perevod;
+                Console.WriteLine("Перевод в рубли:" + Rub);
+            }
+            Console.ReadLine();
+        }
+    }
+}
