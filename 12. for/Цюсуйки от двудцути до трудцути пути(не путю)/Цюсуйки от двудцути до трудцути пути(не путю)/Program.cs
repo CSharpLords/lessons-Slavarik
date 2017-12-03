7eb5ec281a6f1069817ed3369a9d58680d3b5a4c
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-12-03 15:34:36 +0300

    Цусуйки

---
+++ 12. for/Цюсуйки от двудцути до трудцути пути(не путю)/Цюсуйки от двудцути до трудцути пути(не путю)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 20; a < 36; a = a + 1)
+            {
+                Console.WriteLine("Ваше число: " + a);
+            }
+            Console.ReadLine();
+        }
+    }
+}
