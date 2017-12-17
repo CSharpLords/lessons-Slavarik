85c0ba290bdcc2fb9c18b9daaad5701b2dcc2e5b
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-12-17 16:50:26 +0300

    Сумма чисел массива

---
+++ 14. arrays/Сумма чисел массива/Сумма чисел массива/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] massif = { 14, 123, 111, 10 };
+            int sum = 0;
+            for (int num = 0; num < massif.Length; num = num + 1)
+            {
+                sum = sum + massif[num];
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
