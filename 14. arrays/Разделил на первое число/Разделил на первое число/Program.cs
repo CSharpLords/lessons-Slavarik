02110bdea817463a908c90b97fa9bf8723fff730
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-12-17 16:43:16 +0300

    Разделил числа на первое
    Я в шоке

---
+++ 14. arrays/Разделил на первое число/Разделил на первое число/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] massiv = { 32,123,512,123,123,43,32 };
+            int n = 0;
+            int a = massiv.Length - 1;
+            while(n < massiv.Length){
+                double c = massiv[n] / massiv[0];
+                Console.WriteLine(c);
+                n = n + 1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
