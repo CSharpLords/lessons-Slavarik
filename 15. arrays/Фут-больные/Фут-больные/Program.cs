b75c9bc3228df8eba3e43676a88d79ed5b53ff50
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-12-24 15:25:06 +0300

    Фут-больные

---
+++ 15. arrays/Фут-больные/Фут-больные/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Massif = { 1, 3, 2, 3, 5, 6, 7, 8, 4, 5, 6, 2, 9, 1, 2, 6, 3, 5, 2, 1 };
+            for (int n = 0; n < Massif.Length; n++)
+            {
+                if (Massif[n] < 3)
+                {
+                    Console.WriteLine("У команды:" + n + "-меньше 3 побед ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
