b0b91cd5700e02eb16857295756ed1ef1cf6d6f6
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-12-24 16:07:41 +0300

    Поиск чёт чисел

---
+++ 15. arrays/Поиск чёт чисел/Поиск чёт чисел/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1,2,3,4,5,6};
+            for (int n = 0; n < array.Length; n++)
+            {
+                int Rest = array[n] % 2;
+                if (Rest == 0)
+                {
+                    Console.WriteLine("Чёт число найдено и его порядковый номер:" + n);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
