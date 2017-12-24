b139af0dfc8389150302fcdba39ab02ec9a99d57
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-12-24 16:41:01 +0300

    Замена чисел,которые > 10 на их корень

---
+++ 15. arrays/замена на квадрат корень в массиве/замена на квадрат корень в массиве/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace замена_на_квадрат_корень_в_массиве
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = {1.2 , 10.5 , 11.6 , 17.5 };
+            for (int n = 0; n < array.Length; n++)
+            {
+                if (array[n] > 10)
+                {
+                    double Square = Math.Sqrt(array[n]);
+                    Console.WriteLine("Корень из " + array[n] + "-равен:" + Square);
+                    Console.WriteLine();
+                    array[n] = Square;
+                    
+                }
+            }
+            foreach (double element in array)
+            {
+                Console.WriteLine(element);
+            }
+            Console.ReadLine();
+        }
+    }
+}
