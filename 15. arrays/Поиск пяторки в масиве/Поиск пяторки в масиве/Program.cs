4147573235bd2793f1c60a2bcf4cc4694757100b
Slavarik <32611201+Slavarik@users.noreply.github.com>
2017-12-24 15:19:22 +0300

    Поиск пяторки в массиве

---
+++ 15. arrays/Поиск пяторки в масиве/Поиск пяторки в масиве/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Massif = { 4, 1, 2, 3, 5, 4, 5 };
+            for (int n = 0; n < Massif.Length; n = n + 1) 
+            {
+                if (Massif[n] == 5)
+                {
+                    Console.WriteLine("Мы нашли 5 в массиве,и его порядковый номер:" + n);
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
