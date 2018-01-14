086a17035a9a28fdf97fc129a547c4cd93cf1247
Slavarik <32611201+Slavarik@users.noreply.github.com>
2018-01-14 17:06:39 +0300

    Тестовый Unity

---
+++ 16/Тестовый Проект/Assets/scripts/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Test : MonoBehaviour 
+{
+	float time = 0;
+	// Use this for initialization
+	void Start () {
+		print ("Hello world");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print ("update");
+		print ("Координаты:" + "x = " + transform.position.x + ", y = " + transform.position.y + ", z = " + transform.position.z);
+		time = time + Time.deltaTime;
+		int seconds = (int) time;
+		print ("Количество секунд: " + seconds); 
+	}
+}
