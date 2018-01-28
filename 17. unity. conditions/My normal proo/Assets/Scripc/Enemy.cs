cebdf2956737ec627b1188eff5279dea9b11da7a
Slavarik <32611201+Slavarik@users.noreply.github.com>
2018-01-28 16:07:48 +0300

    Враг и его растояние

---
+++ 17. unity. conditions/My normal proo/Assets/Scripc/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	public Transform enemy;
+
+	void Start () {		
+	}
+
+
+	void Update () {
+		float dist = Vector3.Distance(enemy.position, transform.position);
+		print (dist);
+	}
+}
