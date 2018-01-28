910b72ab9383336a3374777c2fd0f6fab25e1508
Slavarik <32611201+Slavarik@users.noreply.github.com>
2018-01-28 16:58:18 +0300

    Макс и Мин высота самолёта

---
+++ 17. unity. conditions/Import/Assets/Scrips/MaxHigh.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaxHigh : MonoBehaviour {
+	float high = 0;
+	float min = 0;
+
+	void Update () {
+		if (transform.position.y > high) {
+			print ("Максимальная высота:" + high);
+			high = transform.position.y;
+		}
+		if (transform.position.y < min) {
+			print ("Минимальная высота:" + min);
+			min = transform.position.y;
+		}
+	}
+}
