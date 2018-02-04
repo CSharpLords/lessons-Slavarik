60d8a311032051fbe1d1e5a125e7fdea2d4c2fcd
Slavarik <32611201+Slavarik@users.noreply.github.com>
2018-02-04 16:43:44 +0300

    Телепортация и предмет

---
+++ 17. unity. conditions/News/Assets/Scrips/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	public Transform enemy;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		print (enemy.position.x);
+		print (enemy.position.y);
+		print (enemy.position.z);
+	}
+}
---
+++ 17. unity. conditions/News/Assets/Scrips/TestSuperMegaTeleport.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSuperMegaTeleport : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {		
+		float x = 0;
+		float z = 0;
+		transform.position = Vector3.down;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
