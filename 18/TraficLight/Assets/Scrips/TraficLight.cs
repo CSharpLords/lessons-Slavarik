89e55e2eb28c500d8ba86f58eacf95ee05294a8b
Slavarik <32611201+Slavarik@users.noreply.github.com>
2018-02-11 16:17:33 +0300

    TraficLight по стрелкам

---
+++ 18/TraficLight/Assets/Scrips/TraficLight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraficLight : MonoBehaviour {
+	Renderer rend;
+	void Start () {
+		rend = GetComponent<Renderer>();
+	}
+
+	void Update () {
+		if (Input.GetKey(KeyCode.DownArrow)){
+			rend.material.color = Color.red;
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			rend.material.color = Color.yellow;
+		}
+		if (Input.GetKey(KeyCode.UpArrow)){
+			rend.material.color = Color.green;
+		}
+	}
+}
