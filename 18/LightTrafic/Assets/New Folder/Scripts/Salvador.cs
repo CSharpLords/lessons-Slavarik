20883753f79849e6f6427e50375cabfae5fec455
Slavarik <32611201+Slavarik@users.noreply.github.com>
2018-02-11 17:00:09 +0300

    LightTrafic по секуднам

---
+++ 18/LightTrafic/Assets/New Folder/Scripts/Salvador.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Salvador : MonoBehaviour {
+	float time = 0;
+	Renderer rend;
+	void Start () {
+		rend = GetComponent<Renderer>();
+	}
+	void Update () {
+		time = time + Time.deltaTime;
+		int seconds = (int)time; 
+		if (seconds == 1) {
+			rend.material.color = Color.red;
+		}
+		if (seconds == 2) {
+			rend.material.color = Color.yellow;
+		}
+		if (seconds == 3) {
+			rend.material.color = Color.green;
+			time = 0;
+		}
+	}
+}
