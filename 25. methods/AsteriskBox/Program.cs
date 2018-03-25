d8a4cf3523ba2174065a1a3b2d9174af43b01c01
Slavarik <32611201+Slavarik@users.noreply.github.com>
2018-03-25 16:42:48 +0300

    Доделал2

--- 25. methods/AsteriskBox/Program.cs
+++ 25. methods/AsteriskBox/Program.cs
@@ -3,8 +3,28 @@
 namespace AsteriskBox {
 	class Program {
 		static void Main(string[] args) {
-			PrintAsteriskBox(20, 10);
+			PrintAsteriskBox(2, 2);
 			Console.ReadLine();
 		}
+        static void PrintAsterisks(int amount)
+        {
+            string star = "";
+            for (int n = 0; n < amount; n++)
+            {
+                if (n < amount)
+                {
+                    star = star + "*";
+                }
+            }
+            Console.WriteLine(star);
+        }
+
+        static void PrintAsteriskBox(int a, int b)
+        {
+            for (int n = 0; n < b; n++)
+            {
+                PrintAsterisks(a);
+            }
+        }
 	}
 }
